435ef45fded4edb894eb0531f84b92daeb44f482
Anna Tarasenko <anna.tarasenko.qa@gmail.com>
2024-02-12 17:06:41 +0200

    HW14

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,5 @@
 package org.example;
+
 import org.example.transport.Bike;
 import org.example.transport.Bus;
 import org.example.transport.Car;
@@ -7,23 +8,16 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.Month;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Vector;
-import java.util.stream.Stream;
-
-import static java.time.temporal.ChronoUnit.DAYS;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
-    private static final String fileName = "resources/transport.csv";
+    private static Logger logger;
+    private static final String fileName = "src/main/resources/transport.csv";
 
     static String[] readFileUsingBufferedReader(String filename) {
-
         ArrayList<String> data = new ArrayList<String>();
         FileReader reader;
 
@@ -47,107 +41,42 @@ public class Main {
         return Arrays.copyOf(data.toArray(), data.size(), String[].class);
     }
 
-    private static List<LandTrans> createTransportObject(String[] data) {
-        List<LandTrans> trans = new Vector<>();
-        getTransportData(data, trans);
-        return trans;
+    public static void main(String[] args) {
+        System.setProperty("java.util.logging.config.file", System.getenv("CONFIG_FILENAME"));
+        logger = Logger.getLogger(Main.class.getName());
+
+        logger.log(Level.CONFIG, "Starting to read data from file: " + fileName);
+        String[] data = readFileUsingBufferedReader(fileName);
+        logger.log(Level.CONFIG, "Read data from file: " + fileName);
+        createTransportObject(data);
+        logger.log(Level.CONFIG, "Finished processing, imported " + (data.length - 1) + " files");
     }
 
-    private static void getTransportData (String[] data, List<LandTrans> trans) {
+    private static void createTransportObject(String[] data) {
         for (int c = 1; c < data.length; c++) {
             var pieces = data[c].split(";"); // method split - щоб "побити" строку на підстроку
             LandTrans transport = null;
-            switch (pieces[1].toLowerCase()) {
-                case "bus":
-                    transport = new Bus(Integer.parseInt(pieces[0]), pieces[2], Integer.parseInt(pieces[3]), Integer.parseInt(pieces[4]), Boolean.parseBoolean(pieces[5]));
-                    break;
-                case "car":
-                    transport = new Car(Integer.parseInt(pieces[0]), pieces[2], Integer.parseInt(pieces[3]), Integer.parseInt(pieces[4]), Boolean.parseBoolean(pieces[5]));
-                    break;
-                case "bike":
-                    transport = new Bike(Integer.parseInt(pieces[0]), pieces[2], Integer.parseInt(pieces[3]), Integer.parseInt(pieces[4]), Boolean.parseBoolean(pieces[5]));
-                    break;
-                default:
-                    transport = new LandTrans(Integer.parseInt(pieces[0]), pieces[2], Integer.parseInt(pieces[3]), Integer.parseInt(pieces[4]), Boolean.parseBoolean(pieces[5])) {
-                    };
-                    break;
+            try {
+                switch (pieces[1].toLowerCase()) {
+                    case "bus":
+                        Bus bus = new Bus(Integer.parseInt(pieces[0]), pieces[2], Integer.parseInt(pieces[3]), Integer.parseInt(pieces[4]), Boolean.parseBoolean(pieces[5]));
+                        logger.log(Level.SEVERE, "Created new Bus: " + bus);
+                        break;
+                    case "car":
+                        Car car = new Car(Integer.parseInt(pieces[0]), pieces[2], Integer.parseInt(pieces[3]), Integer.parseInt(pieces[4]), Boolean.parseBoolean(pieces[5]));
+                        logger.log(Level.FINE, "Created new Car: " + car);
+                        break;
+                    case "bike":
+                        Bike bike = new Bike(Integer.parseInt(pieces[0]), pieces[2], Integer.parseInt(pieces[3]), Integer.parseInt(pieces[4]), Boolean.parseBoolean(pieces[5]));
+                        logger.log(Level.INFO, "Created new Bike: " + bike);
+                        break;
+                    default:
+                        logger.log(Level.WARNING, "Unknown type of transport: " + pieces[1]);
+                        break;
+                }
+            } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+                logger.log(Level.SEVERE, "Failed to create transport object: " + e.getMessage(), e);
             }
-            trans.add(transport);
         }
-    }
-
-    interface MyFunctionalInterface {
-        void doSomething();
-    }
-
-    static MyFunctionalInterface myFunctionalInterface = () -> {
-        System.out.println("Hello!");
-    };
-
-    public static void main(String[] args) throws IOException{
-        var importStart = System.currentTimeMillis();
-        List<LandTrans> trans = new Vector<>();
-        String[] data = readFileUsingBufferedReader(fileName);
-        var importEnd = System.currentTimeMillis() - importStart;
-
-        var createStart = System.currentTimeMillis();
-        trans = createTransportObject(data);
-        var createEnd = System.currentTimeMillis() - createStart;
-
-        var actStreamStart = System.currentTimeMillis();
-        Stream<LandTrans> stream = trans.stream();
-        Stream<LandTrans> stream1 = trans.stream();
-        Stream<LandTrans> stream2 = trans.stream();
-
-        stream
-                .sorted((d1, d2) -> (int) (d2.getCapacity() - d1.getCapacity()))
-                .skip(5)
-                .limit(21)
-                .forEach(System.out::println);
-
-        System.out.println(" "); // щоб була відстань між даними
-        System.out.println("***"); // щоб була відстань між даними
-
-        stream1
-                .map(landTrans -> landTrans.getId() + " " + landTrans.getName() + " " + landTrans.getCapacity())
-//                .limit(21) // можна розкоментувати, якщо потрібно вивести певну кіл-сть рядків, але в ТЗ не було сказано нічого про ліміт у map
-                .forEach(System.out::println);
-
-        System.out.println(" "); // щоб була відстань між даними
-        System.out.println("***"); // щоб була відстань між даними
-
-        stream2
-                .filter(landTrans -> landTrans.getYearEnding() > 2035)
-                .limit(21)
-                .forEach(System.out::println);
-
-        var actStreamEnd = System.currentTimeMillis() - actStreamStart;
-
-//        myFunctionalInterface.doSomething();
-
-        // Вимірювання тривалості
-        System.out.println(" "); // щоб була відстань між даними
-        System.out.println("***"); // щоб була відстань між даними
-
-        Map<String, Long> executionTimes = new LinkedHashMap<>();
-        executionTimes.put("Час імпорту даних з файлу", importEnd);
-        executionTimes.put("Час обробки даних та створення об'єктів", createEnd);
-        executionTimes.put("Час дії з 3 потоками", actStreamEnd);
-
-        executionTimes.entrySet().stream()
-                .sorted(Map.Entry.comparingByValue())
-                .forEach(entry -> System.out.println(entry.getKey() + ": " + entry.getValue() + " мілісекунд"));
-
-        // Вимірювання к-сть днів між датами:
-        System.out.println(" "); // щоб була відстань між даними
-        System.out.println("***"); // щоб була відстань між даними
-
-        LocalDateTime dateBefore = LocalDateTime.of(2023, Month.NOVEMBER, 17, 10, 30);
-        LocalDateTime dateAfter = LocalDateTime.of(2024, Month.FEBRUARY, 11, 10, 20);
-        long daysBetween = DAYS.between(dateBefore, dateAfter);
-
-        System.out.println("Дата до 1 грудня 2023: " + dateBefore);
-        System.out.println("Дата після 1 лютого 2024: " + dateAfter);
-        System.out.println("Різниця у днях: " + daysBetween + " днів.");
     }
 }
